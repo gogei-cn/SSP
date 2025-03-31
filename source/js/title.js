a98debbce0dad1bf6ba6ee66ff8909dbf51bf97b
gogei-cn <zhangyichi20101022@163.com>
2025-03-31 21:46:44 +0800

    modified:   _config.next.yml modified:   _config.yml modified:   db.json modified:   package-lock.json modified:   package.json modified:   source/_data/footer.njk modified:   source/_data/head.njk modified:   source/_data/styles.styl modified:   source/_data/variables.styl modified:   source/js/Slow.js modified:   source/js/canvas-nest.js modified:   source/js/title.js new file:   source/js/verify.js modified:   source/study/index.md

--- source/js/title.js
+++ source/js/title.js
@@ -1,13 +1,13 @@
 var OriginTitile = document.title;
 var titleTime;
 document.addEventListener("visibilitychange", function () {
-  if (document.hidden) {
-    document.title = "???你在哪里???快点回来啊!!!";
-    clearTimeout(titleTime);
-  } else {
-    document.title = "欢迎回来~~";
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
-  }
+    if (document.hidden) {
+        document.title = "???你在哪里???快点回来啊!!!";
+        clearTimeout(titleTime);
+    } else {
+        document.title = "欢迎回来~~";
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
 });
---
+++ source/js/verify.js
@@ -0,0 +1,31 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function verify() {
+    var password = getCookie("password");
+    if (password != "9652530") {
+        password = prompt("请输入密码:", "");
+        if (password == "9652530") {
+            setCookie("password", password, 30);
+        } else {
+            alert("密码错误！");
+            location.reload();
+        }
+    }
+}
+
+verify();
